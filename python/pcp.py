db0751122d36e914b95d657b0ed4223a4f17acc7
Subhodeep Moitra <subhodeep.moitra@gmail.com>
2013-11-23 16:59:16 -0500

    Some bug fixes
    Tried variants of PCP and a number of hyper parameters

--- python/pcp.py
+++ python/pcp.py
@@ -8,6 +8,7 @@ import argparse
 import numpy as np
 import Tkinter
 from PIL import Image, ImageTk
+import scipy.io as sio
 
 def animate(csv_file, w, h):
 	tk_win = Tkinter.Tk()
@@ -43,13 +44,19 @@ def pcp(matrix):
 	S = np.zeros(matrix.shape)
 	Y = np.zeros(matrix.shape)
 	n1, n2 = matrix.shape
-	mu = (n1 * n2) / float(4 * np.max(np.sum(np.abs(matrix), axis=0)))
-	lbda = 1
+	#mu = (n1 * n2) / float(4 * np.max(np.sum(np.abs(matrix), axis=0)))
+	lbda = 1e-2
+	mu = 1e-4
 
+	iter = 1
 	while True:
+		#import pdb ; pdb.set_trace()
+		print("-"*10 + "Iteration %d"%(iter)+ "-"*10) ; iter += 1
 		U, evs, V = np.linalg.svd(matrix - S - ((1/mu) * Y))
-		L_new = U * thresh(evs, mu) * V
-		S_new = thresh(matrix - L_new + ((1/mu) * Y), mu * lbda)
+		#TODO : BUGGY np.diag
+		eps = np.zeros(matrix.shape) ; eps[:matrix.shape[1],:matrix.shape[1]] = np.diag(thresh(evs,mu))
+		L_new = np.dot(np.dot(U,eps),V)
+		S_new = thresh(matrix - L_new + ((1/mu) * Y), lbda * mu)
 		Y_new = Y + mu * (matrix - L_new - S_new)
 
 		if converged(matrix, L_new, S_new):
@@ -57,29 +64,49 @@ def pcp(matrix):
 
 		S = S_new
 		Y = Y_new
+		#import pdb ; pdb.set_trace() 
+ 
+#	return L_new, S_new
+ 
 
 def test_pcp():
 	def pcp_and_print(matrix):
 		print 'M:'
 		print matrix
 		L, S = pcp(matrix)
-
+        
 		print 'L:'
 		print L
 
 		print 'S:'
 		print S
 
-	M = np.asarray([[10 for x in range(10)] for y in range(10)])
-	pcp_and_print(M)
+		import pdb ; pdb.set_trace() 
+  
 
-	N = np.zeros((10, 10))
-	np.fill_diagonal(N, 5)
-	pcp_and_print(N)
+#	M = np.asarray([[10 for x in range(10)] for y in range(10)])
+##	pcp_and_print(M)
+#
+#	N = np.zeros((10, 10))
+#	np.fill_diagonal(N, 5)
+##	pcp_and_print(N)
+#	
+#	O = M
+#	np.fill_diagonal(O, 5)
+#	#import pdb ; pdb.set_trace() 
+
+	#M = sio.loadmat("escalator_data.mat")['X'] #10*np.asarray(np.ones((100,100)))
+	#M = M[5000:10000,:]
+
+	#import pdb ; pdb.set_trace() 
+   
+ 	M  = 10*np.ones((100,100))
+	m = np.random.rand(100,100) 
+	m[m>0.99] = 10.0 ; m[m<0.99] = 0
+	M += m
+  
+	pcp_and_print(M)
 	
-	O = M
-	np.fill_diagonal(O, 5)
-	pcp_and_print(O)
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
